e1ca98a8e3dc6b4375baaffe7e736a55e74ace3b
Progwml6 <progwml6@gmail.com>
2014-01-06 16:22:28 -0500

    update build.gradle, add in several commands from gaz's bot

---
+++ commands/#FTB/ppack.cmd
@@ -0,0 +1,2 @@
+To create a private pack please read the information on the following links.
+Please read all of the pinned topics at the following link: http://forum.feed-the-beast.com/forum/other-third-party-modpacks.48/
