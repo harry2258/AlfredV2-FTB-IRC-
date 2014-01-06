ffdf416509663e7a8a58cd188132a7273ed8a37f
Progwml6 <progwml6@gmail.com>
2014-01-06 17:27:39 -0500

    add in untested implementation for new CHstatus will fix utf8/ascii issues shortly

--- src/main/java/com/harry2258/Alfred/commands/Chstatus.java
+++ src/main/java/com/harry2258/Alfred/commands/Chstatus.java
@@ -11,8 +11,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
-import org.pircbotx.Colors;
 
+import org.pircbotx.Colors;
 import org.pircbotx.hooks.events.MessageEvent;
 
 
@@ -30,6 +30,7 @@ public class Chstatus extends Command {
         
         ArrayList<String> chRepos= new ArrayList<String>();
         ArrayList<String> chURLs= new ArrayList<String>();
+        ArrayList<String> chURLNames= new ArrayList<String>();
         ArrayList<Boolean> tests = new ArrayList<Boolean>();
         ArrayList<String> Status = new ArrayList<String>();
         ArrayList<String> Message = new ArrayList<String>();
@@ -49,6 +50,7 @@ public class Chstatus extends Command {
                         if(splitEntry.length == 2) {
                             chRepos.add(splitEntry[0]);
                             chURLs.add(splitEntry[1]);
+                            chURLNames.add(splitEntry[1].substring(0, splitEntry[1].indexOf(".creeperrepo.net") - 1));
                         }
                     }
                 }
@@ -66,9 +68,9 @@ public class Chstatus extends Command {
                     }
                 }
                 
-                for (Boolean status : tests) {
-                    if(status.equals(true) ) {
-                        Status.add(Colors.DARK_GREEN + "✓" + Colors.NORMAL + " | ");
+                for (int x = 0; x< tests.size(); x++) {
+                    if(tests.get(x).equals(true) ) {
+                        Status.add(Colors.DARK_GREEN + "✓" + " "+ getPercents(chURLNames,x) + "%" + Colors.NORMAL + " | ");
                     } else {
                         Status.add(Colors.RED + "✘" + Colors.NORMAL + " | ");
                     }
@@ -87,7 +89,37 @@ public class Chstatus extends Command {
         return true;
         }
         
-
+    public int getPercents(ArrayList<String> chURLNames, int x){
+      String jsonURL = new String("http://status.creeperrepo.net/fetchjson.php?l=" + chURLNames.get(x));
+      try {
+          URL url;
+          url = new URL(jsonURL);
+          BufferedReader re = new BufferedReader(new InputStreamReader(url.openStream()));
+          String st;
+          
+          while ((st = re.readLine()) != null) {
+              //tests.add(JsonUtils.isJSONObject(st));
+              st = st.replace("{", "").replace("}", "").replace("\"", "");
+              String[] splitString = st.split(",");
+              for(String entry : splitString) {
+                  String[] splitEntry = entry.split(":");
+                  if(splitEntry.length == 2) {
+                      //chRepos.add(splitEntry[0]);
+                      //chURLs.add(splitEntry[1]);
+                      if(splitEntry[0].equals("Bandwidth")){
+                          return Integer.parseInt(splitEntry[1]) * 100 / 1000000;
+                      }
+                  }
+              }
+          }
+      } catch (IOException E) {
+          if (E.getMessage().contains("503")) {
+          }
+          if (E.getMessage().contains("404")) {
+          }
+      }
+      return 0;
+    }
     @Override
     public void setConfig(Config config) {
         this.config = config;
