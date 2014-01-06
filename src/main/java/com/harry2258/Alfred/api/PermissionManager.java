f7cf22bdca3e824c5c224b0daebda5a884e9e4b3
Hardik Desai <diya0206@gmail.com>
2014-01-06 01:02:24 -0800

    New permission, old goes bye bye :D

--- src/main/java/com/harry2258/Alfred/api/PermissionManager.java
+++ src/main/java/com/harry2258/Alfred/api/PermissionManager.java
@@ -4,18 +4,10 @@
  */
 package com.harry2258.Alfred.api;
 
-import com.harry2258.Alfred.Main;
 import org.pircbotx.User;
 import org.pircbotx.Channel;
 
-import java.io.*;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.json.JSONObject;
 
 public class PermissionManager {
@@ -26,81 +18,6 @@ public class PermissionManager {
     
     public PermissionManager(Config conf) {
         this.configs = conf;
-    }
-
-    public void load() {
-        try {
-            properties = new Properties();
-            File config = new File("permissions.properties");
-            if (!config.exists()) {
-                System.out.println("[!!] No configuration file found! generating a new one! [!!]");
-                config.createNewFile();
-                BufferedReader s = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("/permissions.properties")));
-                String tmp = "";
-                BufferedWriter out = new BufferedWriter(new FileWriter(config));
-                while ((tmp = s.readLine()) != null) {
-                    out.write(tmp);
-                    out.flush();
-                    out.newLine();
-                } 
-                out.close();
-                s.close();
-                System.out.println("[!!] Done! [!!]");
-            }
-            
-            modproperties = new Properties();
-            File modconfig = new File("modpermissions.properties");
-            //hur copy from above
-            if (!modconfig.exists()) {
-                System.out.println("[!!] No Mod properties file found! generating a new one! [!!]");
-                modconfig.createNewFile();
-                BufferedReader s = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("/modpermissions.properties")));
-                String tmp = "perms: ";
-                BufferedWriter out = new BufferedWriter(new FileWriter(modconfig));
-                while ((tmp = s.readLine()) != null) {
-                    out.write(tmp);
-                    out.flush();
-                    out.newLine();
-                } 
-                out.close();
-                s.close();
-                System.out.println("[!!] Done! [!!]");
-            }
-            properties.load(new FileInputStream("permissions.properties"));
-            modproperties.load(new FileInputStream("modpermissions.properties"));
-        } catch (Exception ex) {
-            Logger.getLogger(PermissionManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        System.out.println("\n\n=======================\nPERMISSIONS\n=======================");
-        for (String s : properties.stringPropertyNames()) {
-            List<String> permissions = Arrays.asList(properties.getProperty(s).split(" "));
-            System.out.println(s + ": " + permissions.toString());
-        }
-        System.out.println("\n\n=======================\nMOD PERMISSIONS\n=======================");
-        for (String s : modproperties.stringPropertyNames()) {
-            List<String> permissions = Arrays.asList(modproperties.getProperty(s).split(" "));
-            System.out.println(s + ": " + permissions.toString());
-        }
-    }
-    public void reload() throws FileNotFoundException, IOException{
-        //moar copypasta :3
-        properties.load(new FileInputStream("permissions.properties"));
-        modproperties.load(new FileInputStream("modpermissions.properties"));
-        
-        System.out.println("\n\n=======================\nPERMISSIONS\n=======================");
-        for (String s : properties.stringPropertyNames()) {
-            List<String> permissions = Arrays.asList(properties.getProperty(s).split(" "));
-            System.out.println(s + ": " + permissions.toString());
-        }
-        
-        
-        System.out.println("\n\n=======================\nMOD PERMISSIONS\n=======================");
-        for (String s : modproperties.stringPropertyNames()) {
-            List<String> permissions = Arrays.asList(modproperties.getProperty(s).split(" "));
-            System.out.println(s + ": " + permissions.toString());
-        }
-        
     }
 
     public boolean hasPermission(String permission, User user, Channel channel) throws Exception {
